82d168f12e1f4e13b46a65cc175b0c62f06b9167
Obsidian <ObsidianRock@users.noreply.github.com>
2016-11-29 15:50:37 +0000

    mongo queue p3

--- mongo_queue.py
+++ mongo_queue.py
@@ -12,11 +12,42 @@ class MongoQueue:
         self.db = self.client.cache2
         self.timeout = timeout
 
+
+
     def push(self, url):
         try:
             self.db.crawl_queue.insert({'_id': url, 'status': self.OUTSTANDING})
         except errors.DuplicateKeyError as e:
             pass
+
+    def pop(self):
+
+        item = self.db.crawl_queue.find_and_modify(
+            query={'status': self.OUTSTANDING},
+            update={'$set': {'status': self.PROCESSING, 'timestamp': datetime.now()}}
+            )
+
+        if item:
+            return item['_id']
+        else:
+            self.repair()
+            raise KeyError()
+
+    def repair(self):
+
+        record = self.db.crawl_queue.find_and_modify(
+            query={
+                'timestamp': {'$lt': datetime.now() - timedelta(seconds=self.timeout)},
+                'status': {'$ne': self.COMPLETE}
+            },
+            update={'$set': {'status': self.OUTSTANDING}}
+        )
+        # $lt selects the documents where the value of the field is less than
+        # the specified value.
+
+        if record:
+            print('Released:', record['_id'])
+
 
     def peek(self):
         item = self.db.crawl_queue.find_one({'status': self.OUTSTANDING})
